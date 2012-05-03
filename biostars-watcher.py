041805d738e10388803de87a76b5587b4d5f406b
Nathan Nutter <iam@nnutter.com>
2012-05-03 15:39:48 -0500

    add support to create issues in jira (that do not already exist)

--- biostars-watcher.py
+++ biostars-watcher.py
@@ -3,8 +3,10 @@
 import argparse
 import feedparser
 import yaml
+import json
 from urlparse import urlparse
 from jira.client import JIRA
+from requests.auth import HTTPBasicAuth
 
 
 def parse_args():
@@ -23,19 +25,29 @@ def load_config(config_file_path):
     return config
 
 
-def list_issues(config_file_path):
-    config = load_config(config_file_path)
+def jira_issue_exists(jira, entry):
+    url = urlparse(entry.guid)
+    post_id = find_post_id_from_url(url)
+    issues = jira.search_issues(jql_str='labels = BioStars AND labels = ' + post_id)
+    if len(issues) > 0:
+        return True
+    else:
+        return False
+
+
+def connect_jira(config):
     options = {
-        'server': config['jira_server_base_url'],
+        'server': config['jira']['base_url'],
         'basic_auth': {
-            'username': config['username'],
-            'password': config['password'],
+            'username': config['jira']['username'],
+            'password': config['jira']['password'],
         },
     }
     jira = JIRA(options)
-    issues = jira.search_issues(jql_str=config['jql_query'])
-    for issue in issues:
-        print("ISSUE: %s" % (issue.fields.summary))
+    auth_url = config['jira']['base_url'] + '/rest/auth/1/session'
+    auth_data = HTTPBasicAuth(config['jira']['username'], config['jira']['password'])
+    jira._session.get(auth_url, auth=auth_data)
+    return jira
 
 
 def find_post_id_from_url(url):
@@ -44,17 +56,45 @@ def find_post_id_from_url(url):
     return post_id
 
 
-def list_posts(config_file_path):
-    config = load_config(config_file_path)
-    biostars_feed_url = config['biostars_feed_url']
+def issue_creation_data(config, entry):
+    url = urlparse(entry.guid)
+    post_id = find_post_id_from_url(url)
+    fields = {
+        'fields': {
+            'labels': [
+                'BioStars',
+                post_id,
+            ],
+            'summary': entry.title,
+            'description': entry.description,
+            'project': {
+                'key': config['jira']['project_key']
+            },
+            "issuetype": {
+                "id": config['jira']['issue_type_id']
+            },
+        },
+    }
+    return json.dumps(fields)
+
+
+def create_issue(jira, entry, config):
+    payload = issue_creation_data(config, entry)
+    rest_url = config['jira']['base_url'] + '/rest/api/2/issue'
+    r = jira._session.post(rest_url, data=payload)
+    print r.text
+
+
+def check_posts(config):
+    jira = connect_jira(config)
+    biostars_feed_url = config['biostars']['feed_url']
     d = feedparser.parse(biostars_feed_url)
     for entry in d.entries:
-        url = urlparse(entry.guid)
-        post_id = find_post_id_from_url(url)
-        print("POST: (%s) %s" % (post_id, entry.title))
+        if not jira_issue_exists(jira, entry):
+            create_issue(jira, entry, config)
 
 
 if __name__ == '__main__':
     args = parse_args()
-    list_issues(args.c)
-    list_posts(args.c)
+    config = load_config(args.c)
+    check_posts(config)
