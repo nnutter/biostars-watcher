faa362492d749a947aa52de8243b3b139f29315b
Nathan Nutter <iam@nnutter.com>
2012-05-02 22:29:59 -0500

    add command line arg (-c)

--- biostars-watcher.py
+++ biostars-watcher.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
+
+import argparse
 import feedparser
 import yaml
 from urlparse import urlparse
 from jira.client import JIRA
 
 
-def list_issues():
-    config_file = file('config.yaml')
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='biostars-watcher.py',
+    )
+    parser.add_argument('-c', default='config.yaml', help='a config file')
+
+    args = parser.parse_args()
+    return args
+
+
+def list_issues(config_file_path):
+    config_file = file(config_file_path)
     config = yaml.load(config_file)
     options = {
         'server': config['jira_server_base_url'],
@@ -35,5 +48,6 @@ def list_posts():
 
 
 if __name__ == '__main__':
-    list_issues()
+    args = parse_args()
+    list_issues(args.c)
     list_posts()
